47fb2fde3ce0abd81693982ce4467689a17e7b3e
Michael Belozerskiy <m.belozerskiy@yandex.ru>
2018-01-08 23:45:03 +0300

    test commit

---
+++ dev.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/no-extraneous-dependencies */
+require('babel-register');
+/* eslint-enable import/no-extraneous-dependencies */
+
+require('./src/app.js');
---
+++ src/app.js
@@ -0,0 +1,4 @@
+import { get } from "lodash";
+
+const a = 2;
+console.log("Hello, World!", get);
